0a196668b1a45db351a75cc426c65282dc084bb8
Lucas Macedo <ti6@evolusom.com.br>
2022-01-24 09:52:35 -0300

    Add Docker-compose Selenium

--- app/MyScreenshot.php
+++ app/MyScreenshot.php
@@ -44,7 +44,7 @@ class MyScreenshot
 
     $capabilities = DesiredCapabilities::chrome()->setCapability(ChromeOptions::CAPABILITY, $options);
     $driver = retry(5, function () use ($capabilities) {
-      return RemoteWebDriver::create('http://localhost:9515', $capabilities);
+      return RemoteWebDriver::create('http://selenium:4444', $capabilities);
     }, 50);
     $browser = new Browser($driver);
 
@@ -52,34 +52,36 @@ class MyScreenshot
     $size = new WebDriverDimension(1920, 1080);
     $browser->driver->manage()->window()->setSize($size);
 
-    $browser->visit($url);
+    try {
+      $browser->visit($url);
 
-    //Resize to full height for a complete screenshot
-    $body = $browser->driver->findElement(WebDriverBy::tagName('body'));
-    if (!empty($body)) {
-      $currentSize = $body->getSize();
+      //Resize to full height for a complete screenshot
+      $body = $browser->driver->findElement(WebDriverBy::tagName('body'));
+      if (!empty($body)) {
+        $currentSize = $body->getSize();
 
-      //optional: scroll to bottom and back up, to trigger image lazy loading
-      $browser->driver->executeScript('window.scrollTo(0, ' . $currentSize->getHeight() . ');');
-      $browser->pause(1000); //wait a sec
-      $browser->driver->executeScript('window.scrollTo(0, 0);'); //scroll back to top of the page
+        //optional: scroll to bottom and back up, to trigger image lazy loading
+        $browser->driver->executeScript('window.scrollTo(0, ' . $currentSize->getHeight() . ');');
+        $browser->pause(1000); //wait a sec
+        $browser->driver->executeScript('window.scrollTo(0, 0);'); //scroll back to top of the page
 
-      //set window to full height
-      $size = new WebDriverDimension(1920, $currentSize->getHeight()); //make browser full height for complete screenshot
-      $browser->driver->manage()->window()->setSize($size);
+        //set window to full height
+        $size = new WebDriverDimension(1920, $currentSize->getHeight()); //make browser full height for complete screenshot
+        $browser->driver->manage()->window()->setSize($size);
+      }
+
+      $browser->pause(3000); //wait for 3s to give everything time to finish loading - probably better to actually check
+      $image = $browser->driver->TakeScreenshot(); //$image is now the image data in PNG format
+      $browser->driver->quit();
+
+      //save the image somewhere useful
+      $filename = date('Y-m-d-H-i-s') . '.png';
+      file_put_contents(storage_path('app/public/evus_relatorio/') . $filename, $image);
+
+      return file_exists(storage_path('app/public/evus_relatorio/') . $filename) ? storage_path('app/public/evus_relatorio/') . $filename : false;
+
+    } catch (\Exception $e) {
+      $browser->driver->quit();
     }
-
-    $browser->pause(3000); //wait for 3s to give everything time to finish loading - probably better to actually check
-
-    $image = $browser->driver->TakeScreenshot(); //$image is now the image data in PNG format
-
-    $browser->driver->quit();
-    //save the image somewhere useful
-    $filename = date('Y-m-d-H-i-s') . '.png';
-    file_put_contents(storage_path('app/public/evus_relatorio/') . $filename, $image);
-
-    return file_exists(storage_path('app/public/evus_relatorio/') . $filename) ? storage_path('app/public/evus_relatorio/') . $filename : false;
-
   }
-
 }
